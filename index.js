a1c08455ddef30548576bf27047f113ed946d619
reditall <reditall>
2024-07-31 11:07:37 +0200

    refactor

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 #!/usr/bin/env node
 
-
 import chalk from "chalk";
 import boxen from "boxen";
 import yargs from "yargs/yargs";
 import { listTask, addTask, changeTaskStatus, deleteTask } from "./ai.js";
 
-//const greetingV2 = `Hello, ${options.name}!`;
+// const greetingV2 = `Hello, ${options.name}!`;
 
-const greeting = chalk.white.bold("Hello, I'm ara cli ");
+const greeting = chalk.white.bold("Hello, I'm ara cli");
 const log = console.log;
 
 const boxenOptions = {
@@ -17,29 +16,34 @@ const boxenOptions = {
   borderStyle: "round",
   borderColor: "red",
   backgroundColor: "#555555"
-
 };
 
 const msgBox = boxen(greeting, boxenOptions);
 
-
-var argv = yargs(process.argv.slice(2))
+const argv = yargs(process.argv.slice(2))
   .usage(msgBox + 'Usage: $0 <command> [options]')
-  .command('ara', 'Il doit repondre à une question')
-  .example('$0 -a "taskname"', 'add task')
-  .example('$0 -c 1', 'complete task id 1')
+  .command('add', 'Add a task')
+  .command('list', 'List all tasks')
+  .command('complete', 'Complete a task')
+  .command('uncomplete', 'Mark a task as uncompleted')
+  .command('delete', 'Delete a task')
+  .example('$0 -a "taskname"', 'Add a task')
+  .example('$0 -c 1', 'Complete task with id 1')
   .alias('a', 'add')
   .nargs('a', 1)
-  .describe('a', 'Add task')
+  .describe('a', 'Add a task')
   .alias("l", "list")
   .nargs("l", 0)
-  .describe("l", "list Tasks")
+  .describe("l", "List all tasks")
   .alias("d", "delete")
   .nargs("d", 1)
-  .describe("d", "delete Tasks")
+  .describe("d", "Delete a task")
+  .alias("c", "complete")
+  .nargs("c", 1)
+  .describe("c", "Complete a task")
   .alias("u", "uncomplete")
   .nargs("u", 1)
-  .describe("u", "uncomplete Tasks")
+  .describe("u", "Mark a task as uncompleted")
   .help('h')
   .alias('h', 'help')
   .epilog('Redouane ELMALKI copyright 2021')
@@ -48,14 +52,20 @@ var argv = yargs(process.argv.slice(2))
 const qa = argv.a;
 const complete = true;
 const uncomplete = false;
-if (argv.a != undefined)
+
+if (argv.a !== undefined) {
   addTask(qa);
-if (argv.l != undefined)
+}
+if (argv.l !== undefined) {
   listTask();
-if (argv.c != undefined)
+}
+if (argv.c !== undefined) {
   changeTaskStatus(argv.c, complete);
-if (argv.u != undefined)
-  changeTaskStatus(argv.u,uncomplete);
-if (argv.d != undefined)
+}
+if (argv.u !== undefined) {
+  changeTaskStatus(argv.u, uncomplete);
+}
+if (argv.d !== undefined) {
   deleteTask(argv.d);
+}
 
